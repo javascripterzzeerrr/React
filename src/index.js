55e5c2d6afcc4b5234fbc5e3ed3becbc16916066
Nikita <stroganov_n777@mail.ru>
2022-08-14 12:00:07 +0300

    basics of react js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/app';
+
+// const text = 'Hello world!';
+
+// const elem = (
+//   <div>
+//     <h2 className="h2">Текст: {text}</h2>
+//     <input type="text" />
+//     <button />
+//   </div>
+// );
+
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
